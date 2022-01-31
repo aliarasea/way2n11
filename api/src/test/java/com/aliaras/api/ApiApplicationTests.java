9e6b2b13c51b41b3f77a2e10a7fc1ed757cee5bd
Ali Aras <aliaras@outlook.com>
2022-01-31 22:31:36 +0300

    -init

--- api/src/test/java/com/aliaras/api/ApiApplicationTests.java
+++ api/src/test/java/com/aliaras/api/ApiApplicationTests.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class Way2N11ApplicationTests {
+class ApiApplicationTests {
 
     @Test
     void contextLoads() {
